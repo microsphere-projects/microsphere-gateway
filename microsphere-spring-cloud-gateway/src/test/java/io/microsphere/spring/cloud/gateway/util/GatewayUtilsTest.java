91d5882c45043b0fe62284e0fa827d0c4bf0989d
Mercy Ma <mercyblitz@gmail.com>
2025-11-01 09:27:27 +0800

    Refactor gateway property constants and improve tests
    Centralized the gateway enabled property name in GatewayPropertyConstants and updated ConditionalOnGatewayEnabled to use it. Removed unnecessary aliasing and matchIfMissing attribute. Enhanced GatewayUtilsTest with additional tests for route properties and improved setup using JUnit 5 features.

--- microsphere-spring-cloud-gateway/src/test/java/io/microsphere/spring/cloud/gateway/util/GatewayUtilsTest.java
+++ microsphere-spring-cloud-gateway/src/test/java/io/microsphere/spring/cloud/gateway/util/GatewayUtilsTest.java
@@ -18,13 +18,21 @@
 package io.microsphere.spring.cloud.gateway.util;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.env.MockEnvironment;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import static io.microsphere.spring.cloud.gateway.util.GatewayUtils.getGatewayProperties;
+import static io.microsphere.spring.cloud.gateway.util.GatewayUtils.getRouteProperties;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.cloud.gateway.config.GatewayProperties.PREFIX;
 
@@ -37,16 +45,45 @@ import static org.springframework.cloud.gateway.config.GatewayProperties.PREFIX;
  */
 class GatewayUtilsTest {
 
+    private MockEnvironment mockEnvironment;
+
+    @BeforeEach
+    void setUp() {
+        this.mockEnvironment = new MockEnvironment();
+    }
+
     @Test
     void testGetGatewayProperties() {
-        MockEnvironment mockEnvironment = new MockEnvironment();
-        Map<String, Object> gatewayProperties = getGatewayProperties(mockEnvironment);
+        this.mockEnvironment = new MockEnvironment();
+        Map<String, Object> gatewayProperties = getGatewayProperties(this.mockEnvironment);
         assertTrue(gatewayProperties.isEmpty());
 
         String propertyName = PREFIX + ".enabled";
-        mockEnvironment.setProperty(propertyName, "true");
-        gatewayProperties = getGatewayProperties(mockEnvironment);
+        this.mockEnvironment.setProperty(propertyName, "true");
+        gatewayProperties = getGatewayProperties(this.mockEnvironment);
         assertEquals(1, gatewayProperties.size());
         assertEquals("true", gatewayProperties.get("enabled"));
+    }
+
+    @Test
+    void testGetRouteProperties() throws IOException {
+        String routeId = "web-endpoint-mapping";
+        Map<String, Object> routeProperties = getRouteProperties(mockEnvironment, routeId);
+        assertTrue(routeProperties.isEmpty());
+
+        YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+        ClassPathResource classPathResource = new ClassPathResource("META-INF/config/default/test.yaml");
+        List<PropertySource<?>> newPropertySources = loader.load(classPathResource.getPath(), classPathResource);
+        for (PropertySource<?> propertySource : newPropertySources) {
+            mockEnvironment.getPropertySources().addLast(propertySource);
+        }
+
+        mockEnvironment.setProperty(PREFIX + ".routes[1].id", "xforwarded_remoteaddr_route");
+        routeProperties = getRouteProperties(mockEnvironment, routeId);
+        assertFalse(routeProperties.isEmpty());
+
+        routeId += "-1";
+        routeProperties = getRouteProperties(mockEnvironment, routeId);
+        assertTrue(routeProperties.isEmpty());
     }
 }
