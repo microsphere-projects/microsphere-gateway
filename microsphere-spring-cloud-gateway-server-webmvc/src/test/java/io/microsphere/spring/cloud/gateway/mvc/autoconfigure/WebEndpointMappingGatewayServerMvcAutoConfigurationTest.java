1d103e4ca6448a95fca9819bbebac21021e9bd06
Mercy Ma <mercyblitz@gmail.com>
2025-11-12 17:38:16 +0800

    Enhance test for WebEndpointMappingHandlerConfig
    Expanded the test to cover RouteProperties refresh and validation of findWebEndpointMappingRouteProperties with different key sets. This improves test coverage for route property mapping logic.

--- microsphere-spring-cloud-gateway-server-webmvc/src/test/java/io/microsphere/spring/cloud/gateway/mvc/autoconfigure/WebEndpointMappingGatewayServerMvcAutoConfigurationTest.java
+++ microsphere-spring-cloud-gateway-server-webmvc/src/test/java/io/microsphere/spring/cloud/gateway/mvc/autoconfigure/WebEndpointMappingGatewayServerMvcAutoConfigurationTest.java
@@ -23,9 +23,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.server.mvc.config.RouteProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.Set;
+
+import static io.microsphere.collection.Lists.ofList;
+import static io.microsphere.collection.Sets.ofSet;
+import static io.microsphere.spring.cloud.gateway.mvc.constants.GatewayPropertyConstants.GATEWAY_ROUTES_PROPERTY_NAME_PREFIX;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link WebEndpointMappingGatewayServerMvcAutoConfiguration} Test
@@ -49,8 +58,25 @@ class WebEndpointMappingGatewayServerMvcAutoConfigurationTest {
     @Autowired
     private WebEndpointMappingHandlerConfig webEndpointMappingHandlerConfig;
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void test() {
         assertNotNull(webEndpointMappingHandlerConfig);
+
+        RouteProperties routeProperties = new RouteProperties();
+        routeProperties.setId("unknown-route-id");
+        this.webEndpointMappingHandlerConfig.refresh(ofList(routeProperties), this.context, handlerFilterFunction -> {
+        });
+
+
+        Set<String> keys = ofSet("test-property-name");
+        assertTrue(this.webEndpointMappingHandlerConfig.findWebEndpointMappingRouteProperties(keys).isEmpty());
+
+        keys = ofSet(GATEWAY_ROUTES_PROPERTY_NAME_PREFIX + "[0].id",
+                GATEWAY_ROUTES_PROPERTY_NAME_PREFIX + "[0].metadata",
+                GATEWAY_ROUTES_PROPERTY_NAME_PREFIX + "[1].id");
+        assertFalse(this.webEndpointMappingHandlerConfig.findWebEndpointMappingRouteProperties(keys).isEmpty());
     }
 }
