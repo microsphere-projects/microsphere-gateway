d0400b9891564b7f4b21f6a923b4ad0aa757546f
Mercy Ma <mercyblitz@gmail.com>
2025-12-05 22:09:08 +0800

    Add Nacos Config Server integration and tests
    Introduces NacosConfigServerAutoConfiguration and NacosEnvironmentRepository to support Nacos as a backend for Spring Cloud Config Server. Updates dependencies in pom.xml, registers auto-configuration, and adds integration tests using Testcontainers with a Docker Compose setup for Nacos.

---
+++ microsphere-spring-cloud-gateway-commons/src/main/java/io/microsphere/spring/cloud/gateway/commons/config/server/nacos/environment/NacosEnvironmentRepository.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.cloud.gateway.commons.config.server.nacos.environment;
+
+
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.cloud.nacos.NacosConfigProperties;
+import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Properties;
+
+import static com.alibaba.nacos.api.common.Constants.DEFAULT_GROUP;
+import static io.microsphere.lang.function.ThrowableSupplier.execute;
+import static java.lang.String.format;
+
+/**
+ * Nacos {@link EnvironmentRepository}.
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 0.2.0
+ */
+public class NacosEnvironmentRepository implements EnvironmentRepository {
+
+    private final NacosConfigProperties nacosConfigProperties;
+
+    private final ConfigService configService;
+
+    public NacosEnvironmentRepository(NacosConfigManager nacosConfigManager, NacosConfigProperties nacosConfigProperties) {
+        this.nacosConfigProperties = nacosConfigProperties;
+        this.configService = nacosConfigManager.getConfigService();
+    }
+
+    @Override
+    public Environment findOne(String application, String profile, String label) {
+
+        String dataId = application + "-" + profile + ".properties";
+
+        String configContent = execute(() -> configService.getConfig(dataId, DEFAULT_GROUP, this.nacosConfigProperties.getTimeout()));
+
+        return createEnvironment(configContent, application, profile);
+    }
+
+    private Environment createEnvironment(String configContent, String application,
+                                          String profile) {
+
+        Environment environment = new Environment(application, profile);
+
+        Properties properties = createProperties(configContent);
+
+        String propertySourceName = format("Nacos[application : %s , profile : %s]", application, profile);
+
+        PropertySource propertySource = new PropertySource(propertySourceName,
+                properties);
+
+        environment.add(propertySource);
+
+        return environment;
+    }
+
+    private Properties createProperties(String configContent) {
+        Properties properties = new Properties();
+        if (StringUtils.hasText(configContent)) {
+            try {
+                properties.load(new StringReader(configContent));
+            } catch (IOException e) {
+                throw new IllegalStateException("The format of content is a properties");
+            }
+        }
+        return properties;
+    }
+}
---
+++ microsphere-spring-cloud-gateway-commons/src/test/java/io/microsphere/spring/cloud/gateway/commons/config/server/nacos/autoconfigure/NacosConfigServerAutoConfigurationIntegrationTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.cloud.gateway.commons.config.server.nacos.autoconfigure;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.junit.jupiter.EnabledIfDockerAvailable;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage;
+
+/**
+ * {@link NacosConfigServerAutoConfiguration} Integration Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see NacosConfigServerAutoConfiguration
+ * @since 1.0.0
+ */
+@EnabledIfSystemProperty(named = "testcontainers.enabled", matches = "true")
+@EnabledIfDockerAvailable
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        NacosConfigServerAutoConfigurationIntegrationTest.class
+})
+@TestPropertySource(properties = {
+        "spring.application.name=test",
+})
+@EnableAutoConfiguration
+class NacosConfigServerAutoConfigurationIntegrationTest {
+
+    private static ComposeContainer composeContainer;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        ClassLoader classLoader = NacosConfigServerAutoConfigurationIntegrationTest.class.getClassLoader();
+        URL resource = classLoader.getResource("META-INF/docker/service-registry-servers.yml");
+        File dockerComposeFile = new File(resource.toURI());
+        composeContainer = new ComposeContainer(dockerComposeFile);
+        composeContainer.waitingFor("nacos", forLogMessage(".*Nacos started successfully.*", 1))
+                .start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        composeContainer.stop();
+    }
+
+    @Test
+    void test() {
+
+    }
+}
